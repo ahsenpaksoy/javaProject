7dfadd3b1ac9db47a3c46b4686dafeb96a3bc81b
ahsenpaksoy <ahsenpaksoy@gmail.com>
2022-08-10 18:04:15 +0200

    calculator

--- day36_staticClassMembers/CalculatorTest.java
+++ day36_staticClassMembers/CalculatorTest.java
@@ -10,9 +10,38 @@ public class CalculatorTest {
 		Calculator c = new Calculator();
 		System.out.println("Result:" + c.plus(55, 7));
 		
+		int i = Integer.parseInt("33");
+		System.out.println(Character.isDigit('5'));
 		
+		CalculatorTest.m1();
+		
+		CalculatorTest C1 = new CalculatorTest();
+		C1.m1();
+		
+		m1();
+		
+		//static methods are called as ClassName.methodName(); or 
+		//methodName(); only if in the same class
+		
+		// Aciklama eklenecek
 		
 		
 	}
+	
+	public static void m1() {
+		System.out.println("hello");
+	}
+	
+	public static void m2() {
+		System.out.println("java");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
